0006e6359d7fb0c4514d0731ac72739b2ead2d8b
Jared Easterday <jared.easterday@gmail.com>
2017-07-15 18:19:37 -0700

    got a good pattern established

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const {createStore} = require('redux');
 const battleApp = require('./reducers');
 const actions = require('./actions');
 
-
 const createNewWarrior = () => ({
   id: new Date().toISOString(),
   name: 'Warrior',
@@ -16,32 +15,8 @@ const createNewWarrior = () => ({
   symbol: '⚔'
 })
 
-/*  
-  You may optionally specify the initial state as the second 
-  argument to createStore(). This is useful for hydrating the state of the client 
-  to match the state of a Redux application running on the server.
-
-  let store = createStore(todoApp, window.STATE_FROM_SERVER)
-
-  We could do that here to get state from a db
-*/
-
-
-let store = createStore(battleApp);
-
-const listener = () => {
-  console.log('the state of the store has changed: ', store.getState());
-}
-
-const unsubscribe = store.subscribe(listener); 
-
-// example of a dispatch
-// store.dispatch(actions.addUnit(createNewWarrior()))
-
-console.log('we have a store: ', store.getState())
-
-// Start the server at port 8080
-var server = http.createServer(function(req, res){ 
+const store = createStore(battleApp);
+const server = http.createServer( (req, res) => { 
   // Send HTML headers and message
   res.writeHead(200,{ 'Content-Type': 'text/html' }); 
   res.end('<h1>Hello Socket Lover!</h1>');
@@ -50,50 +25,33 @@ var server = http.createServer(function(req, res){
 server.listen(8080);
 
 // Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
+const socket = io.listen(server);
 socket.set('origins', 'http://localhost:3000');
 
-const players = [];
+// Subscribe to Redux
+const listener = () => {
+  socket.emit('change', store.getState())
+}
 
-// Add a connect listener
-socket.on('connection', function(client){ 
+const unsubscribe = store.subscribe(listener);
 
-  socket.emit('hydrateStore', store.getState())
+socket.on('connection', client => { 
+  console.log('client connection')
 
-  // Success!  Now listen to messages to be received
-  client.on('turn',function(event){ 
-    console.log('Received message from client!',event);
-  });
+  client.on('getState', () => {
+    socket.emit('change', store.getState())
+  })
 
-  // Testing idea of reacting to emits
-  // Listen for emit, then dispatch action
   client.on('addUnit', (unit, location) => {
-
-    socket.emit('addUnit', unit, location);
-
-    // Okay so we're getting units in server redux state now
     store.dispatch(actions.addUnit(unit))
     store.dispatch(actions.setUnitLocation(unit.id, location))
   })
 
   client.on('setUnitLocation', (unitId, location) => {
-    socket.emit('setUnitLocation', unitId, location);
+    store.dispatch(actions.setUnitLocation(unitId, location));
   })
 
-  client.on('player', player => {
-
-    if (players.length < 2) {
-      players.push(player)
-      socket.emit('playerAdded', player)
-    } else {
-      socket.emit('error', 'too many players')
-    }
-
-    socket.emit('players', players);
-    console.log('players', players);
-  })
-
-  client.on('disconnect', function() {
+  client.on('disconnect', () => {
     console.log('Client has disconnected');
   });
 
