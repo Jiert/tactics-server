e99b2420cb717237ac48468c00aeb2f9d2bfea0c
Jared Easterday <easterday@planet.com>
2017-08-26 13:10:04 -0700

    Upating movesLeft for units on turn change

--- reducers/units.js
+++ reducers/units.js
@@ -1,5 +1,5 @@
-const units = (state = {}, action) => { 
-  switch (action.type) {  
+const units = (state = {}, action) => {
+  switch (action.type) {
     case 'ADD_UNIT':
       return Object.assign({}, state, {
         [action.payload.id]: action.payload
@@ -8,16 +8,27 @@ const units = (state = {}, action) => {
     case 'UPDATE_UNIT':
       return Object.assign({}, state, {
         [action.payload.id]: Object.assign(
-          {}, 
-          state[action.payload.id], 
+          {},
+          state[action.payload.id],
           action.payload.updates
         )
-      })
+      });
+
+    case 'SET_ACTIVE_PLAYER': {
+      const newState = Object.assign({}, state);
+
+      Object.keys(newState).forEach(key => {
+        if (newState[key].commanderId === action.payload) {
+          newState[key].movesLeft = newState[key].mobility;
+        }
+      });
+
+      return newState;
+    }
 
     default:
-      return state
+      return state;
   }
 };
 
 module.exports = units;
-
