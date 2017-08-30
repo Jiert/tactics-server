7e66ab24cec27cca024636c5b56177afc68033dd
Jared Easterday <jared.easterday@gmail.com>
2017-08-29 23:20:36 -0700

    add file for generating units

---
+++ units.js
@@ -0,0 +1,22 @@
+module.exports = {
+  createNewWarrior: commanderId => ({
+    id: Date.now(),
+    name: 'Warrior',
+    maxHealth: 10,
+    health: 10,
+    mobility: 2,
+    movesLeft: 0,
+    symbol: '⚔',
+    commanderId
+  }),
+
+  createNewCastle: commanderId => ({
+    id: Date().now(),
+    name: 'Castle',
+    maxHealth: 100,
+    health: 100,
+    mobility: 0,
+    symbol: '🏰',
+    commanderId
+  })
+};
