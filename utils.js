98284fed30b33ef50c19323bdf88710c542d9a78
Jared Easterday <jared.easterday@gmail.com>
2017-09-03 16:08:19 -0700

    new terrain map

--- utils.js
+++ utils.js
@@ -15,14 +15,56 @@ const terrainTypes = {
   forest: {
     name: 'Forest',
     movementPenalty: 2
+  },
+  mountains: {
+    name: 'Mountains',
+    movementPenalty: 3
   }
 };
 
 const terrain = {
-  [15.11]: terrainTypes.plains,
-  [14.11]: terrainTypes.forest,
-  [16.11]: terrainTypes.forest,
-  [`${14}.${10}`]: terrainTypes.hills
+  ['9.14']: terrainTypes.forest,
+  ['10.14']: terrainTypes.forest,
+  ['10.13']: terrainTypes.forest,
+  ['13.13']: terrainTypes.forest,
+  ['11.14']: terrainTypes.forest,
+  ['12.14']: terrainTypes.forest,
+  ['11.12']: terrainTypes.forest,
+  ['11.11']: terrainTypes.forest,
+  ['12.11']: terrainTypes.forest,
+  ['13.10']: terrainTypes.forest,
+  ['14.10']: terrainTypes.forest,
+  ['14.12']: terrainTypes.forest,
+  ['15.9']: terrainTypes.forest,
+  ['16.11']: terrainTypes.forest,
+  ['18.10']: terrainTypes.forest,
+  ['19.9']: terrainTypes.forest,
+  ['20.9']: terrainTypes.forest,
+  ['21.8']: terrainTypes.forest,
+  ['22.7']: terrainTypes.forest,
+  ['22.6']: terrainTypes.forest,
+  ['21.6']: terrainTypes.forest,
+  ['20.6']: terrainTypes.forest,
+  ['19.7']: terrainTypes.forest,
+  ['18.7']: terrainTypes.forest,
+  ['17.8']: terrainTypes.forest,
+  [`${11}.${13}`]: terrainTypes.mountains,
+  [`${12}.${13}`]: terrainTypes.mountains,
+  [`${12}.${12}`]: terrainTypes.mountains,
+  [`${13}.${12}`]: terrainTypes.mountains,
+  [`${13}.${11}`]: terrainTypes.mountains,
+  [`${14}.${11}`]: terrainTypes.mountains,
+  [`${15}.${11}`]: terrainTypes.mountains,
+  [`${15}.${10}`]: terrainTypes.mountains,
+  [`${16}.${10}`]: terrainTypes.mountains,
+  [`${17}.${10}`]: terrainTypes.mountains,
+  [`${17}.9`]: terrainTypes.mountains,
+  [`${18}.9`]: terrainTypes.mountains,
+  [`${19}.8`]: terrainTypes.mountains,
+  ['18.8']: terrainTypes.mountains,
+  ['20.8']: terrainTypes.mountains,
+  ['20.7']: terrainTypes.mountains,
+  ['21.7']: terrainTypes.mountains
 };
 
 const locationsForPlayer = {
